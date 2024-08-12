6cdd0bbdd2b2fd8340b1d0120b7d761c38854388
souravsingha13 <souravsingha1304021@gmail.com>
2024-08-13 00:44:59 +0600

    :construction: Create several models

--- dining_manager/dining/admin.py
+++ dining_manager/dining/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Consumer, ProductCatagory, Products
 
-# Register your models here.
+class ConsumerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')  
+    search_fields = ('name', 'email')  
+
+class ProductCatagoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)  
+    search_fields = ('name',)  
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'catagory')  
+    search_fields = ('name', 'catagory__name')  
+    list_filter = ('catagory',)  
+
+# Register the models with the custom admin classes
+admin.site.register(Consumer, ConsumerAdmin)
+admin.site.register(ProductCatagory, ProductCatagoryAdmin)
+admin.site.register(Products, ProductsAdmin)
--- dining_manager/dining/models.py
+++ dining_manager/dining/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
-
 class Consumer(models.Model):
     name = models.CharField(max_length= 200)
+    email = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
     
+class ProductCatagory(models.Model):
+    name = models.CharField(max_length= 200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length= 200)
+    catagory = models.ForeignKey(ProductCatagory, on_delete=models.CASCADE,  related_name="products")
+
+
+    def __str__(self) -> str:
+        return self.name
+    
+
