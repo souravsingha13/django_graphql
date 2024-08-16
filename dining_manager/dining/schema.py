04fae03fa66acaa977a59d43723af84aaa8a8d96
souravsingha13 <souravsingha1304021@gmail.com>
2024-08-16 13:16:17 +0600

    :sparkles: Create consumer create method

--- dining_manager/dining/schema.py
+++ dining_manager/dining/schema.py
@@ -3,17 +3,32 @@ from graphene_django.types import DjangoObjectType
 from .models import Consumer,ProductCatagory,Products
 
 
-class CustomerType(DjangoObjectType):
+class ConsumerType(DjangoObjectType):
     class Meta:
         model = Consumer
 
-class ProductCatagoryType(DjangoObjectType):
+class ProductCategoryType(DjangoObjectType):
     class Meta:
         model = ProductCatagory
 
 class ProductType(DjangoObjectType):
     class Meta:
         model = Products
+        
+# Query to get the data
+class Query(graphene.ObjectType):
+    consumers = graphene.List(ConsumerType)
+    product_categories = graphene.List(ProductCategoryType)
+    products = graphene.List(ProductType)
+
+    def resolve_consumers(self, info, **kwargs):
+        return Consumer.objects.all()
+
+    def resolve_product_categories(self, info, **kwargs):
+        return ProductCatagory.objects.all()
+
+    def resolve_products(self, info, **kwargs):
+        return Products.objects.all()
 
 
 # Mutations for CRUD operatons
@@ -23,9 +38,46 @@ class CreateConsumer(graphene.Mutation):
         name = graphene.String(required=True)
         email = graphene.String(required=True)
     
-    consumer = graphene.Field(CustomerType)
+    consumer = graphene.Field(ConsumerType)
 
     def mutate(self, info, name, email):
         consumer = Consumer(name=name, email=email)
         consumer.save()
         return CreateConsumer(consumer = consumer)
+    
+
+class UpdateConsumer(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required = True)
+        name = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    consumer = graphene.Field(ConsumerType)
+
+    def mutate(self, info, name, email):
+        consumer = Consumer.objects.get(pk = id )
+        if name :
+            consumer.name = name
+        if email :
+            consumer.email = email
+        consumer.save()
+
+        return UpdateConsumer(consumer = consumer)
+
+class DeleteConsumer(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    consumer = graphene.Field(ConsumerType)
+
+    def mutate(self, info, id):
+        consumer = Consumer.objects.get(pk=id)
+        consumer.delete()
+        return DeleteConsumer(consumer=None)
+    
+class Mutation(graphene.ObjectType):
+    create_consumer = CreateConsumer.Field()
+    update_consumer = UpdateConsumer.Field()
+    delete_consumer = DeleteConsumer.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
